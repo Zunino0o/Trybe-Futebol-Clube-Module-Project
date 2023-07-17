8c82a68b4db5d95d7f801a958b7053530bf0b2c7
Lucas Zunino <zunino10@hotmail.com>
2023-07-17 16:39:25 -0300

    cria model e migration de teams

---
+++ app/backend/src/database/migrations/01-create-teams-table.ts
@@ -0,0 +1,23 @@
+import { DataTypes, Model, QueryInterface } from 'sequelize';
+import { ITeam } from '../../Interfaces/ITeam';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<ITeam>>('teams', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      teamName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+  },
+  
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('teams');
+  },
+};
