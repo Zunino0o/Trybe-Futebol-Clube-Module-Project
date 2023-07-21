513f89fc322b160cc0a2686d0da460598e7a3252
Lucas Zunino <zunino10@hotmail.com>
2023-07-20 21:11:36 -0300

    cria matchesService

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -1,5 +1,12 @@
-import MatchesModel from "../models/MatchesModel";
+import { ServiceResponse } from '../Interfaces/ServiceResponse';
+import MatchesModel from '../models/MatchesModel';
+import { IMatches } from '../Interfaces/matches/IMatches';
 
 export default class MatchesService {
-    constructor(private matchesModel: MatchesModel = new MatchesModel()) {}
+  constructor(private matchesModel: MatchesModel = new MatchesModel()) {}
+
+  public async getAll(): Promise<ServiceResponse<IMatches[]>> {
+    const allMatches = await this.matchesModel.findAll();
+    return { status: 'SUCCESSFUL', data: allMatches };
+  }
 }
