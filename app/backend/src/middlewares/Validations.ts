7bc77eccf2d9861eb72ff52c47f653c2415a932b
Lucas Zunino <zunino10@hotmail.com>
2023-07-25 01:43:47 -0300

    finaliza dia

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -12,10 +12,7 @@ export default class Validations {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
     const emailRegex = /^(?!.*\.\.)[a-zA-Z0-9._%+-]+@(?!.*\.\.)[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
-    if (!emailRegex.test(email)) {
-      return res.status(401).json({ message: 'Invalid email or password' });
-    }
-    if (password.length < 6) {
+    if (!emailRegex.test(email) || password.length < 6) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
     next();
